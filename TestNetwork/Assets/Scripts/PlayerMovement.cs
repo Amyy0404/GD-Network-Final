ecd816863541cad68eee3c99f5ccd12731546c45
saifrancis <2572767@students.wits.ac.za>
2025-05-05 21:28:30 +0200

    animations

--- TestNetwork/Assets/Scripts/PlayerMovement.cs
+++ TestNetwork/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 public class PlayerMovement : NetworkBehaviour
 {
     public float moveSpeed = 2f;
+    private Animator goblinCont;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        goblinCont = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        if (isLocalPlayer)
-        {
-            // Move locally
-            float moveX = Input.GetAxis("Horizontal");
-            float moveY = Input.GetAxis("Vertical");
+        if (!isLocalPlayer) return;
 
-            Vector3 playMovement = new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime;
-            transform.position += playMovement;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 playMovement = new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime;
+        transform.position += playMovement;
+
+     
+        bool isMoving = moveX != 0 || moveY != 0;
+
+        if (goblinCont != null)
+        {
+            goblinCont.SetBool("isWalking", isMoving);
+        }
+
+        if (spriteRenderer != null && moveX != 0)
+        {
+            spriteRenderer.flipX = moveX < 0;
         }
     }
 }
